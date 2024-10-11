f669566fcf8b2e1ca8cca60700434e66a60ef4bb
t3snake <t3snake@gmail.com>
2024-10-11 12:32:34 +0530

    Add basic express app with ts for ex 9.4

---
+++ index.ts
@@ -0,0 +1,13 @@
+import express from 'express'
+
+const app = express()
+
+app.get('/hello', (_request, response) => {
+    response.send('Hello Full Stack!')
+})
+
+const PORT = 3003
+
+app.listen(PORT, () => {
+    console.log(`Server running on Port ${PORT}`)
+})
