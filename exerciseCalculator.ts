aed08050473db0d3030280ff6e11d9bccd72cdbf
t3snake <t3snake@gmail.com>
2024-10-11 13:34:27 +0530

    Use eslint recommended ts settings for ex 9.6

--- exerciseCalculator.ts
+++ exerciseCalculator.ts
@@ -9,7 +9,7 @@ interface Result {
 }
 
 const calculateExercises = (exerciseHours: number[], target: number): Result => {
-    let res: Result = {
+    const res: Result = {
         periodLength: 0,
         trainingDays: 0,
         success: false,
@@ -17,44 +17,45 @@ const calculateExercises = (exerciseHours: number[], target: number): Result => 
         ratingDescription: '',
         target: 0,
         average: 0
-    }
+    };
 
-    res.periodLength = exerciseHours.length
-    res.trainingDays = exerciseHours.filter(hours => hours !== 0).length
-    const sum = exerciseHours.reduce((a, b) => a+b, 0)
-    res.average = sum/exerciseHours.length || 0
-    res.success = res.average >= target
-    res.target = target
-    res.rating = 1 + (res.average/target)
+    res.periodLength = exerciseHours.length;
+    res.trainingDays = exerciseHours.filter(hours => hours !== 0).length;
+    const sum = exerciseHours.reduce((a, b) => a+b, 0);
+    res.average = sum/exerciseHours.length || 0;
+    res.success = res.average >= target;
+    res.target = target;
+    res.rating = 1 + (res.average/target);
 
     if ( res.rating < 1.75 ) {
-        res.ratingDescription = 'Poor performance'
+        res.ratingDescription = 'Poor performance';
     } else if ( res.rating < 2 ) {
-        res.ratingDescription = 'Not bad, just a bit more to succeed'
+        res.ratingDescription = 'Not bad, just a bit more to succeed';
     } else if ( res.rating < 2.25 ) {
-        res.ratingDescription = 'Reached goals! Congrats!'
+        res.ratingDescription = 'Reached goals! Congrats!';
     } else {
-        res.ratingDescription = 'Amazing! Over achieved targets'
+        res.ratingDescription = 'Amazing! Over achieved targets';
     }
 
-    return res
-}
+    return res;
+};
 
 const parseArguments = (args: string[]) => {
     if (args.length < 4) throw new Error('Not enough arguments');
-    let [_arg0, _arg1, target, ...exDays] = args
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const [_arg0, _arg1, target, ...exDays] = args;
     if (isNaN(Number(target))) throw new Error('target not a number');
 
     exDays.forEach(day => {
         if (isNaN(Number(day))) throw new Error('hours are not all numbers');
-    })
+    });
 
     return {
         exerciseHours: exDays.map(day => Number(day)),
         target: Number(target)
-    }
-}
+    };
+};
 
-const {exerciseHours, target} = parseArguments(process.argv)
+const {exerciseHours, target} = parseArguments(process.argv);
 
-console.log(calculateExercises(exerciseHours, target))
+console.log(calculateExercises(exerciseHours, target));
--- index.ts
+++ index.ts
@@ -1,32 +1,32 @@
-import express from 'express'
+import express from 'express';
 
-import { calculateBmi } from './bmiCalculator'
+import { calculateBmi } from './bmiCalculator';
 
-const app = express()
+const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
 app.get('/hello', (_request, response) => {
-    response.send('Hello Full Stack!')
-})
+    response.send('Hello Full Stack!');
+});
 
 app.get('/bmi', (request, response) => {
-    const height = Number(request.query.height)
-    const weight = Number(request.query.weight)
+    const height = Number(request.query.height);
+    const weight = Number(request.query.weight);
 
     if ( !height || !weight || isNaN(weight) || isNaN(height) ) {
-        response.status(400).send({error: 'Malformed query parameters'})
-        return
+        response.status(400).send({error: 'Malformed query parameters'});
+        return;
     }
     response.json({
         weight,
         height,
         bmi: calculateBmi(height, weight)
-    })
-})
+    });
+});
 
-const PORT = 3003
+const PORT = 3003;
 
 app.listen(PORT, () => {
-    console.log(`Server running on Port: ${PORT}`)
-})
+    console.log(`Server running on Port: ${PORT}`);
+});
