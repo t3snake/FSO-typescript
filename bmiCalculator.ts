bb226f9e5eb8da9c8285d7674684b5b5c9b0fea7
t3snake <t3snake@gmail.com>
2024-10-11 13:20:03 +0530

    Add get endpoint for bmi for ex 9.5

--- bmiCalculator.ts
+++ bmiCalculator.ts
@@ -1,16 +1,17 @@
 
-const calculateBmi = (height: number, weight: number) => {
+export const calculateBmi = (height: number, weight: number) => {
     const bmi: number = 10000*weight/(height*height)
 
     if (bmi < 18.5) return 'Under Range'
     if (bmi < 25) return 'Normal Range'
     return 'Over Range'
 }
-
-if (process.argv.length < 4) throw new Error('Not enough arguments');
-if (process.argv.length > 4) throw new Error('Too many arguments');
-if (isNaN(Number(process.argv[2])) || isNaN(Number(process.argv[3]))) throw new Error('Arguments not numbers')
-const height = Number(process.argv[2])
-const weight = Number(process.argv[3])
-
-console.log(calculateBmi(height, weight))
+if (require.main === module){
+    if (process.argv.length < 4) throw new Error('Not enough arguments');
+    if (process.argv.length > 4) throw new Error('Too many arguments');
+    if (isNaN(Number(process.argv[2])) || isNaN(Number(process.argv[3]))) throw new Error('Arguments not numbers')
+    const height = Number(process.argv[2])
+    const weight = Number(process.argv[3])
+    
+    console.log(calculateBmi(height, weight))
+}
