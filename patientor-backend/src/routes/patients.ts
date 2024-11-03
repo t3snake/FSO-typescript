e10f279e75b292308b5f6d4eef9bc2d272fe1b1b
t3snake <t3snake@gmail.com>
2024-11-03 20:26:45 +0530

    Switch to zod handling for request for ex 9.14

--- patientor-backend/src/routes/patients.ts
+++ patientor-backend/src/routes/patients.ts
@@ -1,10 +1,11 @@
 import express from 'express';
 import { Response } from 'express';
+import { z } from 'zod';
 
 import { NewPatient, Patient, SecuredPatient } from '../types';
 
 import patientService from '../services/patientService';
-import { toNewPatient } from '../utils';
+import { newEntrySchema } from '../utils';
 
 const router = express.Router();
 
@@ -16,11 +17,19 @@ router.get('/', (_req, res: Response<SecuredPatient[]>) => {
 });
 
 router.post('/', (req, res) => {
-  console.log('Saving a diary!');
-  const newPatient: NewPatient = toNewPatient(req.body);
-  const patient: Patient = patientService.addPatient(newPatient);
-
-  res.json(patient);
+    try{
+        console.log('Saving a diary!');
+        const newPatient: NewPatient = newEntrySchema.parse(req.body);
+        const patient: Patient = patientService.addPatient(newPatient);
+    
+        res.json(patient);
+    } catch (error: unknown) {
+        if (error instanceof z.ZodError) {      
+            res.status(400).send({ error: error.issues });    
+        } else {      
+            res.status(400).send({ error: 'unknown error' });    
+        }  
+    }
 });
 
 export default router;
--- patientor-backend/src/utils.ts
+++ patientor-backend/src/utils.ts
@@ -1,4 +1,13 @@
+import { z } from 'zod';
 import { Gender, NewPatient } from "./types";
+
+export const newEntrySchema = z.object({
+    name: z.string(),
+    dateOfBirth: z.string().date(),
+    ssn: z.string(),
+    gender: z.nativeEnum(Gender),
+    occupation: z.string(),
+});
 
 export const toNewPatient = (object: unknown): NewPatient => {
     if(!object || !(typeof(object) == 'object') ) {
