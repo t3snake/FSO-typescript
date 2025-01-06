af92251f620614c9b49f2cfc4dd35ebe3c8f3dcb
t3snake <t3snake@gmail.com>
2025-01-06 20:22:02 +0530

    Add Entry types for ex 9.23

--- patientor/backend/src/types.ts
+++ patientor/backend/src/types.ts
@@ -14,10 +14,44 @@ export interface Error {
     error: string;
 }
 
-// eslint-disable-next-line @typescript-eslint/no-empty-object-type
-export interface Entry {
-    
+interface BaseEntry {
+    id: string;
+    description: string;
+    date: string;
+    specialist: string;
+    diagnosisCodes?: Diagnosis['code'][];
 }
+
+export enum HealthCheckRating {
+    'Healthy' = 0,
+    'LowRisk' = 1,
+    'HighRisk' = 2,
+    'CriticalRisk' = 3,
+}
+
+interface HealthCheckEntry extends BaseEntry {
+    type: 'HealthCheck';
+    healthCheckRating: HealthCheckRating;
+}
+
+interface HospitalEntry extends BaseEntry {
+    type: 'Hospital';
+    discharge: {
+        date: string;
+        criteria: string;
+    };
+}
+
+interface OccupationalHealthcareEntry extends BaseEntry {
+    type: 'OccupationalHealthcare';
+    employerName: string;
+    sickLeave?: {
+        startDate: string;
+        endDate: string;
+    };
+}
+
+export type Entry = HealthCheckEntry | HospitalEntry | OccupationalHealthcareEntry;
 
 export interface Patient {
     id: string;
